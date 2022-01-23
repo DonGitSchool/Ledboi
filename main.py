8be6e85f37337b9b03952103783674c88644aa31
DonGitSchool <92173334+DonGitSchool@users.noreply.github.com>
2022-01-23 16:47:45 -0700

    Made the LEDS only go to 100 on each instead of full brightness

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+button_on = False
+
 def on_pin_pressed_p0():
+    global button_on
     pins.analog_write_pin(AnalogPin.P1, 100)
     pins.analog_write_pin(AnalogPin.P2, 100)
     pins.analog_write_pin(AnalogPin.P8, 100)
-    global button_on
     if button_on == True:
         pins.analog_write_pin(AnalogPin.P8, 0)
         pins.analog_write_pin(AnalogPin.P1, 0)
         pins.analog_write_pin(AnalogPin.P2, 0)
         button_on = False
     else:
-        pins.analog_write_pin(AnalogPin.P8, 255)
-        pins.analog_write_pin(AnalogPin.P1, 255)
-        pins.analog_write_pin(AnalogPin.P2, 255)
+        pins.analog_write_pin(AnalogPin.P8, 100)
+        pins.analog_write_pin(AnalogPin.P1, 100)
+        pins.analog_write_pin(AnalogPin.P2, 100)
         if True:
-            pins.analog_write_pin(AnalogPin.P8, 255)
-            pins.analog_write_pin(AnalogPin.P1, 255)
-            pins.analog_write_pin(AnalogPin.P2, 255)
+            pins.analog_write_pin(AnalogPin.P8, 100)
+            pins.analog_write_pin(AnalogPin.P1, 100)
+            pins.analog_write_pin(AnalogPin.P2, 100)
             button_on = True
 input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
 
@@ -31,7 +33,6 @@ def on_button_pressed_b():
     pins.analog_write_pin(AnalogPin.P2, 0)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-button_on = False
 def on_forever():
     pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 let button_on = false
-input.onPinPressed(TouchPin.P0, function () {
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
     pins.analogWritePin(AnalogPin.P1, 100)
     pins.analogWritePin(AnalogPin.P2, 100)
     pins.analogWritePin(AnalogPin.P8, 100)
@@ -9,27 +10,29 @@ input.onPinPressed(TouchPin.P0, function () {
         pins.analogWritePin(AnalogPin.P2, 0)
         button_on = false
     } else {
-        pins.analogWritePin(AnalogPin.P8, 255)
-        pins.analogWritePin(AnalogPin.P1, 255)
-        pins.analogWritePin(AnalogPin.P2, 255)
+        pins.analogWritePin(AnalogPin.P8, 100)
+        pins.analogWritePin(AnalogPin.P1, 100)
+        pins.analogWritePin(AnalogPin.P2, 100)
         if (true) {
-            pins.analogWritePin(AnalogPin.P8, 255)
-            pins.analogWritePin(AnalogPin.P1, 255)
-            pins.analogWritePin(AnalogPin.P2, 255)
+            pins.analogWritePin(AnalogPin.P8, 100)
+            pins.analogWritePin(AnalogPin.P1, 100)
+            pins.analogWritePin(AnalogPin.P2, 100)
             button_on = true
         }
+        
     }
+    
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     pins.analogWritePin(AnalogPin.P8, 255)
     pins.analogWritePin(AnalogPin.P1, 255)
     pins.analogWritePin(AnalogPin.P2, 255)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     pins.analogWritePin(AnalogPin.P8, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
     pins.analogWritePin(AnalogPin.P2, 0)
 })
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
